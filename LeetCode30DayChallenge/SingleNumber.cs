22c24fbd92e42c53eedbccf898052b51b58d9a90
Eddy Bautista <bautista.j.eddy@gmail.com>
2020-04-01 20:42:10 -0500

    * .gitignore: Added First Challenge
    * LeetCode30DayChallenge.sln:
    * Program.cs:
    * SingleNumber.cs:
    * LeetCode30DayChallenge.csproj:

---
+++ LeetCode30DayChallenge/SingleNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace LeetCode30DayChallenge
+{
+    public class SingleNumber
+    {
+        public static int SingleNumberDayOne(int[] nums)
+        {
+            int x = 0;
+            foreach (int num in nums)
+            {
+                x ^= num;
+            }
+
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            int[] dayOne = new int[] { 2, 2, 1 };
+            Console.WriteLine(SingleNumberDayOne(dayOne));
+        }
+    }
+}
